438473279ac5357578f2bb0ea0f5ca03e8ada562
igor atakhanov <igoratakhanov@gmail.com>
2018-02-08 17:57:30 -0500

    Changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res)=>{ // Route handler with get method.
-  res.send({hi:'there'}) // How to respond to this particular handler.
+  res.send({hi:'there you go champ'}) // How to respond to this particular handler.
 })
 
 const PORT = process.env.PORT || 5000 // Heroku env variable
