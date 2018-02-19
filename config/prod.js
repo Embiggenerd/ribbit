e9dec60ea1831f3f80ff8d40cc641a4fbeb31afc
igor atakhanov <igoratakhanov@gmail.com>
2018-02-19 14:17:43 -0500

    google auth done, prod/dev done.
    TODO: actual logic of app

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
+mongodb://igor_prod:pruction_server@ds241668.mlab.com:41668/emaily-prod123
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const cookieSession = require("cookie-session")
+const passport = require("passport")
+const keys = require("./config/keys")
+require("./models/User")
+require("./services/passport")
+
+mongoose.connect(keys.mongoURI)
 
 const app = express()
 
-app.get('/', (req, res)=>{ // Route handler with get method.
-  res.send({hi:'there you go champ'}) // How to respond to this particular handler.
-})
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require("./routes/auth_routes")(app)
+
 
 const PORT = process.env.PORT || 5000 // Heroku env variable
 app.listen(PORT) // Which port to listen on.
