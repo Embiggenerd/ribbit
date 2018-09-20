4555db9b507d2befe2b21953da8ba70dbfb48bf4
igor atakhanov <igoratakhanov@gmail.com>
2018-09-19 21:27:31 -0400

    new config

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID:
+    "968024870389-5mren1d2lrodj35jrve309ladoooburr.apps.googleusercontent.com",
+  googleClientSecret: "vPqtfHVSzpr4tMQ8pnvQxwY0",
+  mongoURI: "mongodb://igor:password@ds235418.mlab.com:35418/emaily-dev123",
+  cookieKey: "kljlkjlkjlkjp",
+  stripePublishableKey: "pk_test_jBlUyPyK47F3i88bFV70gDLA",
+  stripeSecretKey: "sk_test_pvfx8vd9zb2O30gcsroRwieK",
+  sendGridKey: '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',
+  redirectDomain: 'http://localhost:3000'
+}
